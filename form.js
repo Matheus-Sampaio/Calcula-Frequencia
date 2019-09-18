3b88816fbd069add15d62cbad86a5c007460cb63
Matheus-Sampaio <matheusballestra@hotmail.com>
2019-09-18 00:39:59 -0300

    delete form.js

--- form.js
+++
@@ -1,8 +0,0 @@
-var botaoAdicionar = document.querySelector("#botao");
-
-botaoAdicionar.addEventListener("click", function(){
-    event.preventDefault();
-    var form = document.querySelector(".formPessoa");
-    var pessoa = obtemPacienteDoFormulario(form);
-});
-
